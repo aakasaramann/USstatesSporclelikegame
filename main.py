3b4a88872c6421fff7414d7d8b114b23cdc1f867
aakasaramann <bsaikartheek@hotmail.com>
2022-01-04 20:52:05 +0530

    Added highscore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+from datetime import datetime
+
 import pandas
 
 screen = turtle.Screen()
@@ -11,7 +13,7 @@ turtle.shape(image)
 answer_file = pandas.read_csv("50_states.csv")
 state_list = answer_file["state"].tolist()
 
-
+player_name = 'noname'
 game_completed = False
 states_correct = []
 PROMPT = "What's another state name?"
@@ -28,6 +30,16 @@ while not game_completed:
         y_cor = int(answer_file[answer_file.state == answer_state].y)
         t.goto(x_cor, y_cor)
         t.write(answer_state, align='center', font=('Arial', 8, 'normal'))
+    elif answer_state == 'Exit':
+        missing_states = state_list
+        for state in states_correct:
+            missing_states.remove(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States_to_Learn.csv")
+        player_name = screen.textinput(title=f"Your score: {len(states_correct)}", prompt="Enter a name")
+        break
 
+with open("high_score.txt", 'w') as high_score_file:
+    high_score_file.write(f'{player_name} : {len(states_correct)} @ {datetime.now()}\n')
 
-screen.exitonclick()
+# screen.exitonclick()
